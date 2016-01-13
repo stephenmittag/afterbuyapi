d8cf8522e79c005b2eb70e5100a13a135fbd77c6
Thomas Stägemann <thomas@wirkaufens.de>
2016-01-13 16:50:59 +0100

    Added configuration test

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace WK\AfterbuyApi\Tests\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionConfigurationTestCase;
+use Wk\AfterbuyApi\DependencyInjection\Configuration;
+use Wk\AfterbuyApi\DependencyInjection\WkAfterbuyApiExtension;
+
+/**
+ * Class ConfigurationTest
+ */
+class ConfigurationTest extends AbstractExtensionConfigurationTestCase
+{
+    public function testConfigurationFiles()
+    {
+        $expectedConfiguration = [
+            'user' => [
+                'id' => 'user',
+                'password' => 'strong-password',
+            ],
+            'partner' => [
+                'id' => 123456789,
+                'password' => 'strong-password',
+            ],
+            'error_language' => 'de',
+        ];
+
+        $sources = [ __DIR__ . '/../Data/DependencyInjection/config.yml' ];
+
+        $this->assertProcessedConfigurationEquals($expectedConfiguration, $sources);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getContainerExtension()
+    {
+        return new WkAfterbuyApiExtension();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getConfiguration()
+    {
+        return new Configuration();
+    }
+}
