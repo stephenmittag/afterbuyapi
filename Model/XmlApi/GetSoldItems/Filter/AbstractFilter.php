5ef824aa47f44595fee32d610c7725c75a914a28
Thomas Stägemann <thomas@wirkaufens.de>
2016-02-02 16:43:30 +0100

    Added toArray and toString to all filter objects

--- Model/XmlApi/GetSoldItems/Filter/AbstractFilter.php
+++ Model/XmlApi/GetSoldItems/Filter/AbstractFilter.php
@@ -11,19 +11,18 @@ use Wk\AfterbuyApiBundle\Model\XmlApi\AbstractModel;
 abstract class AbstractFilter extends AbstractModel
 {
     /**
-     * @Serializer\Type("string")
-     * @Serializer\SerializedName("FilterName")
-     * @var string
-     */
-    private $filterName;
-
-    /**
      * @Serializer\Type("array<string,string>")
      * @Serializer\XmlKeyValuePairs()
      * @Serializer\SerializedName("FilterValues")
      * @var array
      */
     protected $filterValues;
+    /**
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("FilterName")
+     * @var string
+     */
+    private $filterName;
 
     /**
      * @param string $filterName
@@ -40,4 +39,25 @@ abstract class AbstractFilter extends AbstractModel
     {
         return $this->filterName;
     }
+
+    /**
+     * Converts the object to an array
+     *
+     * @return array
+     */
+    public function __toArray()
+    {
+        return [$this->filterName => $this->filterValues];
+    }
+
+    /**
+     * Converts the object to a string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('%s: %s', $this->filterName, json_encode($this->filterValues));
+    }
 }
+
